cee94d4221c8d3e36828c13a3c909fe52120b3a2
Jason Herrera <baytownjason@yahoo.com>
2021-01-26 21:29:23 -0600

    commit tree cc1ac65847d624414c678b5f8fc7122201c2bf95

--- lib/student.rb
+++ lib/student.rb
@@ -16,6 +16,7 @@ class Student
   end
 
   def self.create_table
+    #creates db
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
@@ -27,6 +28,7 @@ class Student
   end
 
   def self.drop_table
+    #deletes students table
     sql = <<-SQL 
     DROP TABLE students
     SQL
@@ -34,6 +36,7 @@ class Student
   end
 
   def save
+    #updates attributes if id is found or creates row if id isnt found
     if self.id
       self.update
     else
@@ -46,12 +49,14 @@ class Student
   end
 
   def self.create(name, grade)
+    #creates new instance from passed in values & adds to db
     student = Student.new(name, grade)
     student.save
     student
   end
 
   def self.new_from_db(row)
+    #creates instance from db row
     id = row[0]
     name = row[1]
     grade = row[2]
@@ -59,16 +64,20 @@ class Student
   end
 
   def self.find_by_name(name)
+    #find first row in db where name = name
     sql = <<-SQL
     SELECT * FROM students WHERE name = ?
     SQL
     DB[:conn].execute(sql, name).map {|row| new_from_db(row)}.first
-    #binding.pry
   end
 
   def update
-
+    #updates both name & grade when id is passed
+    sql = <<-SQL
+    UPDATE students SET name = ?, grade = ? WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
-
+  
 end
